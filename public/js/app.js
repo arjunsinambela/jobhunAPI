80716cd4651c7b8140bf6ca2c85958f3856b63f1
daffakbar <daffaakbar5@gmail.com>
2019-09-25 17:24:42 +0700

    content
    content

--- public/js/app.js
+++ public/js/app.js
@@ -1729,6 +1729,14 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({});
 
 /***/ }),
@@ -37432,72 +37440,138 @@ var staticRenderFns = [
             },
             [
               _c("h6", { staticClass: "m-0 font-weight-bold text-primary" }, [
-                _vm._v("Earnings Overview")
-              ]),
-              _vm._v(" "),
-              _c("div", { staticClass: "dropdown no-arrow" }, [
-                _c(
-                  "a",
-                  {
-                    staticClass: "dropdown-toggle",
-                    attrs: {
-                      href: "#",
-                      role: "button",
-                      id: "dropdownMenuLink",
-                      "data-toggle": "dropdown",
-                      "aria-haspopup": "true",
-                      "aria-expanded": "false"
-                    }
-                  },
-                  [
-                    _c("i", {
-                      staticClass: "fas fa-ellipsis-v fa-sm fa-fw text-gray-400"
-                    })
-                  ]
-                ),
-                _vm._v(" "),
-                _c(
-                  "div",
-                  {
-                    staticClass:
-                      "dropdown-menu dropdown-menu-right shadow animated--fade-in",
-                    attrs: { "aria-labelledby": "dropdownMenuLink" }
-                  },
-                  [
-                    _c("div", { staticClass: "dropdown-header" }, [
-                      _vm._v("Dropdown Header:")
-                    ]),
-                    _vm._v(" "),
-                    _c(
-                      "a",
-                      { staticClass: "dropdown-item", attrs: { href: "#" } },
-                      [_vm._v("Action")]
-                    ),
-                    _vm._v(" "),
-                    _c(
-                      "a",
-                      { staticClass: "dropdown-item", attrs: { href: "#" } },
-                      [_vm._v("Another action")]
-                    ),
-                    _vm._v(" "),
-                    _c("div", { staticClass: "dropdown-divider" }),
-                    _vm._v(" "),
-                    _c(
-                      "a",
-                      { staticClass: "dropdown-item", attrs: { href: "#" } },
-                      [_vm._v("Something else here")]
-                    )
-                  ]
-                )
+                _vm._v("Tentang Jobhun")
               ])
             ]
           ),
           _vm._v(" "),
-          _c("div", { staticClass: "card-body" }, [
-            _c("div", { staticClass: "chart-area" }, [
-              _c("canvas", { attrs: { id: "myAreaChart" } })
+          _c("h4", { staticStyle: { "text-align": "center" } }, [
+            _c("em", [
+              _vm._v("Jobhun – Career Development Platform for Millenials")
             ])
-          ])
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v(
+              "Jobhun merupakan startup pendukung perkembangan karier masyarakat Indonesia. Kami memiliki layanan utama berupa Jobhun Career Hub dan Jobhun Academy. Melalui platform ini, kami akan mempertemukan pencari kerja dan pemberi kerja dengan cara yang cepat dan mudah. Pencari kerja dapat melamar kerja dengan mudah, sedangkan perusahaan dapat menemukan kandidat yang tepat. Sedangkan Jobhun Academy dibuat guna membekali para calon pekerja dan pekerja dengan berbagai skill yang saat ini sedang dibutuhkan di dunia kerja. Nantinya mereka akan disalurkan oleh Jobhun pada beberapa perusahaan partner Jobhun yang sedang mencari tenaga kerja atau talent."
+            )
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v(
+              "Selain itu, Jobhun juga memiliki beragam program pendukung yang bisa mendukung karier dan skill masyarakat, seperti program magang, talks, hingga visitasi ke perusahaan-perusahaan. Seluruh upaya tersebut kami lakukan untuk mencapai sebuah misi, yaitu mengurangi angka pengangguran di Indonesia. Tak hanya itu saja, kami juga menyediakan berbagai artikel, berita, maupun informasi acara yang dapat menunjang karier para Jobhuners (Sebutan untuk followers Jobhun). Penggunaan situs ini juga dibuat sangat sederhana, sehingga pengguna tidak akan kesulitan saat menggunakannya. Tidak muluk-muluk, bagi kami ‘sharing is caring’."
+            )
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _c("strong", [_vm._v("Visi Jobhun")])
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v(
+              "Menjadi portal penunjang karier nomor satu di Indonesia. Dikenal dan bisa berdampak nyata bagi karier masyarakat Indonesia."
+            )
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _c("strong", [_vm._v("Misi Jobhun")])
+          ]),
+          _vm._v(" "),
+          _c("ol", { staticStyle: { "text-align": "justify" } }, [
+            _c("li", [
+              _vm._v(
+                "Membantu mengurangi angka pengangguran di Indonesia dengan menyediakan layanan dan program yang dapat menunjang karier masyarakat"
+              )
+            ]),
+            _vm._v(" "),
+            _c("li", [
+              _vm._v(
+                "Mengedukasi dan menyebarkan berbagai macam informasi yang berguna untuk perkembangan karier"
+              )
+            ]),
+            _vm._v(" "),
+            _c("li", [
+              _vm._v(
+                "Mendukung peningkatan dan pengembangan kemampuan masyarakat Indonesia sebagai bekal dalam berkarier"
+              )
+            ]),
+            _vm._v(" "),
+            _c("li", [
+              _vm._v(
+                "Membantu masyarakat Indonesia yang masih merasa bingung dalam memilih karier yang tepat bagi mereka dengan menjadi wadah yang mampu memberikan solusi"
+              )
+            ])
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _c("strong", [_vm._v("Layanan Jobhun")])
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v("1. Jobhun Career Hub")
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v(
+              "Sebuah platform yang bisa disebut sebagai talent pool, dimana Jobhun akan mempertemukan calon pekerja atau pekerja dengan perusahaan yang sedang mencari kandidat. Melalui Jobhun Career Hub, pekerja dengan perusahaan yang sedang membuka lowongan kerja akan dipertemukan dengan mudah dan cepat."
+            )
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v("2. Jobhun Academy")
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v(
+              "Jobhun Academy merupakan sebuah program pelatihan yang bertujuan untuk membekali para calon pekerja dan pekerja dengan berbagai skill yang saat ini sedang dibutuhkan di dunia kerja. Nantinya mereka akan disalurkan oleh Jobhun pada beberapa perusahaan partner Jobhun yang sedang mencari tenaga kerja atau talent melalui Jobhun Career Hub."
+            )
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v("3. Media partner")
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v(
+              "Jobhun menerima kerja sama media partner untuk acara-acara yang berhubungan dengan karier maupun penunjang skill dalam berkarier."
+            )
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _c("strong", [_vm._v("Program Jobhun")])
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v("1. Jobhun Internship")
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v(
+              "Sebuah program magang dari Jobhun yang ditujukan bagi para pelajar atau mahasiswa, dengan rentang usia 15-22 tahun. Posisi magang yang dibuka biasanya menyesuaikan kebutuhan proyek Jobhun. Dalam program ini, mereka akan mendapatkan pelatihan atau mentoring beberapa topik dan berkesempatan untuk membantu tim inti Jobhun mengerjakan beberapa proyek dan pekerjaan rutin."
+            )
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v("2. Jobhun Talks")
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v(
+              "Jobhun Talks merupakan sebuah sharing session yang ditujukan untuk umum, biasanya mengambil topik yang berkaitan dengan dunia kerja maupun pengembangan skill. Topik dibawakan oleh orang yang ahli dalam bidangnya, sesuai dengan topik. Inti pembicaraan kegiatan lebih kepada motivasi, tips dan trik, atau gambaran umum sebuah profesi atau hal-hal yang berkaitan dengan dunia karier."
+            )
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v("3. Jobhun Visit")
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } }, [
+            _vm._v(
+              "Jobhun Visit merupakan sebuah program kunjungan ke kantor-kantor atau instansi yang dilakukan oleh tim inti Jobhun, para intern, maupun pengguna yang beruntung. Program ini dibuat untuk memperkenalkan dunia kerja yang sesungguhnya kepada para pencari kerja. Mereka akan diajak melihat secara langsung suasana kerja yang ada di perusahaan-perusahaan, kemudian menggali lebih jauh tentang profesi-profesi yang ada pada perusahaan tersebut."
+            )
+          ]),
+          _vm._v(" "),
+          _c("p", { staticStyle: { "text-align": "justify" } })
         ])
       ])
     ])
