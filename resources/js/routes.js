b1cc31e0bcf107010e86ad64bb73533cd8f6fd97
yunusWidodo <younews372@gmail.com>
2019-09-25 17:23:05 +0700

    rooter
    career hub

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -3,6 +3,11 @@ let routes=[
   path: '/',
   name: '',
   component: require('./components/Content.vue').default
+},
+{
+  path: '/jobhunCareerHub',
+  name: 'jobhunCareerHub',
+  component: require('./components/jobhunCareerHub').default
 }
 ];
 export default routes
