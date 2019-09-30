b02c195009f5e601b32d37ab72384fcb64d1a9a0
Zunedi Suprianto Sinambela <42605944+Zunedi@users.noreply.github.com>
2019-09-30 14:38:57 +0700

    Update

--- resources/js/components/content/layanan/jobhunAcademy.vue
+++ resources/js/components/content/layanan/jobhunAcademy.vue
@@ -1,24 +1,69 @@
 <template>
     <div>
-        <div class="col-md-12">
-            <div class="card shadow mb-4">
-                                    <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Jobhun Academy</h6>
+        <div class="card-deck">
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="asset/img/jobhun_putih.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a
+                            natural lead-in to additional content. This content is a little bit
+                            longer.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
                 </div>
-                <!-- Card Body -->
-                <div class="container">
-                    <h1>Daffa Akbar</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quas sequi quos neque. Laboriosam dolore nemo tenetur commodi, eligendi corporis porro mollitia ex facere voluptas dolorem officiis consequatur, quibusdam maxime?</p>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="asset/img/jobhun_putih.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a
+                            natural lead-in to additional content. This content is a little bit
+                            longer.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
                 </div>
-
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="asset/img/jobhun_putih.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a
+                            natural lead-in to additional content. This content is a little bit
+                            longer.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="asset/img/jobhun_putih.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a
+                            natural lead-in to additional content. This content is a little bit
+                            longer.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {
+    export default {
 
-}
+    }
+
 </script>
--- resources/js/components/content/layanan/jobhunCareerHub.vue
+++ resources/js/components/content/layanan/jobhunCareerHub.vue
@@ -35,73 +35,74 @@
                                     </select>
                                 </div>
                                 <div class="col-md-3">
-                                    <button type="button" class="btn btn-primary"><i class="fa fa-search"> Search</i></button>
+                                    <button type="button" class="btn btn-primary"><i class="fa fa-search">
+                                            Search</i></button>
                                 </div>
                             </div>
-                           
+
                         </form>
-                    </div> 
-                 
+                    </div>
+
                 </div>
             </div>
-                <div class="card-deck">
-                                <div class="col-md-4">
-                                    <div class="card">
-                                        <img class="card-img-top" src="asset/img/jobhun_putih.png"  alt="Card image cap">
-                                        <div class="card-body">
-                                            <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">This is a wider card with supporting text below as a
-                                                natural lead-in to additional content. This content is a little bit
-                                                longer.</p>
-                                        </div>
-                                        <div class="card-footer">
-                                            <small class="text-muted">Last updated 3 mins ago</small>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="card">
-                                        <img class="card-img-top"  src="asset/img/jobhun_putih.png" alt="Card image cap">
-                                        <div class="card-body">
-                                            <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">This is a wider card with supporting text below as a
-                                                natural lead-in to additional content. This content is a little bit
-                                                longer.</p>
-                                        </div>
-                                        <div class="card-footer">
-                                            <small class="text-muted">Last updated 3 mins ago</small>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="card">
-                                        <img class="card-img-top"  src="asset/img/jobhun_putih.png" alt="Card image cap">
-                                        <div class="card-body">
-                                            <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">This is a wider card with supporting text below as a
-                                                natural lead-in to additional content. This content is a little bit
-                                                longer.</p>
-                                        </div>
-                                        <div class="card-footer">
-                                            <small class="text-muted">Last updated 3 mins ago</small>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="card">
-                                        <img class="card-img-top" src="asset/img/jobhun_putih.png" alt="Card image cap">
-                                        <div class="card-body">
-                                            <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">This is a wider card with supporting text below as a
-                                                natural lead-in to additional content. This content is a little bit
-                                                longer.</p>
-                                        </div>
-                                        <div class="card-footer">
-                                            <small class="text-muted">Last updated 3 mins ago</small>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+            <div class="card-deck">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img class="card-img-top" src="asset/img/jobhun_putih.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a
+                                natural lead-in to additional content. This content is a little bit
+                                longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img class="card-img-top" src="asset/img/jobhun_putih.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a
+                                natural lead-in to additional content. This content is a little bit
+                                longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img class="card-img-top" src="asset/img/jobhun_putih.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a
+                                natural lead-in to additional content. This content is a little bit
+                                longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img class="card-img-top" src="asset/img/jobhun_putih.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a
+                                natural lead-in to additional content. This content is a little bit
+                                longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -110,4 +111,5 @@
     export default {
 
     }
+
 </script>
