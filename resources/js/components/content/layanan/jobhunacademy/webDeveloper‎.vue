177602e3877b6a6399cf3dd53c1a1d6ae0312b5c
daffakbar <daffaakbar5@gmail.com>
2019-10-21 14:37:15 +0700

    jobhun academy

---
+++ resources/js/components/content/layanan/jobhunacademy/webDeveloper‎.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="container">
+            <h1>contet warijoeaj</h1>
+            <h1>contet warijoeaj</h1>
+            <h1>contet warijoeaj</h1>
+            <h1>contet warijoeaj</h1>
+            <h1>contet warijoeaj</h1>
+            <h1>contet warijoeaj</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
